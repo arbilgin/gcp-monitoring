61892af5c53679faabf6bec31a6f257d43c43b67
arbilgin <arda.bilgin@blok-z.com>
2024-02-05 20:55:22 +0300

    update login feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
+import os
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, make_response
 from google.cloud import monitoring_v3
 
 from metrics import GoogleMetrics
@@ -17,11 +18,12 @@ users = {
     "blokz": generate_password_hash("Monitor.123!")
 }
 
-session = { "logged_in": False }
+session = { }
 
 def set_session(username):
     session["logged_in"] = True
     session["username"] = username
+    session["token"] = os.urandom(24).hex()  # Generates a random token
 def setup_metrics():
     client = monitoring_v3.MetricServiceClient()
     project = f"projects/greenlink-platform-396912"
@@ -40,11 +42,24 @@ def setup_metrics():
     metrics = GoogleMetrics(client, project, aggregation, interval)
     return metrics
 
+# @app.before_request
+# def before_request():
+#     if "token" in session:
+#         token_cookie = request.cookies.get('session_token')
+#         if token_cookie != session.get('token'):
+#             # The session token cookie doesn't match the session token; log the user out
+#             print ("token cookie: ", token_cookie)
+#             print ("session token: ", session.get('token'))
+#             session.clear()
+#             return redirect("/login")
+
+            
 @auth.verify_password
 def verify_password(username, password):
     if username in users and \
             check_password_hash(users.get(username), password):
         return username
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -52,20 +67,25 @@ def login():
        password = request.form["password"]
        if verify_password(username, password):
            set_session(username)
-           return app.redirect("/")
+           response = make_response(redirect("/"))
+           response.set_cookie('session_token', session['token'], secure=True, httponly=True)
+           return response
+        #    before_request()
        else:
            return "Invalid username or password", 401
+       
     return render_template("login.html")
 
 @auth.login_required
 @app.route("/")
 def index():
-    if session["logged_in"] == True:
-        metrics = setup_metrics()
-        data = metrics.get_data()
-        from pprint import pprint      
-        pprint(data)
-        return render_template("index.html", response=data)
+    token_cookie = request.cookies.get('session_token')
+    if token_cookie == session.get('token'):
+            metrics = setup_metrics()
+            data = metrics.get_data()
+            from pprint import pprint      
+            pprint(data)
+            return render_template("index.html", response=data)
     else:
         return redirect("/login")
 
